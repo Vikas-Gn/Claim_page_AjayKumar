35a6f6e4cd33c42a20f83054f388e955c1574be2
Vikas-Gn <vikasgn2000@gmail.com>
2025-07-21 12:23:52 +0530

    updated-code

--- Backend/server.js
+++ Backend/server.js
@@ -401,7 +401,7 @@ app.use((err, req, res, next) => {
 
 // Start server
 const PORT = 3099;
-const HOST = '65.2.177.203'; // Listen on all interfaces
+const HOST = '0.0.0.0'; // Listen on all interfaces
 
 app.listen(PORT, HOST, async () => {
     console.log(`Server running on http://${HOST}:${PORT}`);
